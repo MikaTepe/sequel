c5c1463cb3f3203bc7905962b01b16ae0a1dcba7
Mika Franz Tepe <mtepe@uni-muenster.de>
2025-09-17 22:27:40 +0200

    fix: added proper app name for debugging

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sequel"
+    namespace = "com.sequel.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sequel"
+        applicationId = "com.sequel.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/sequel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sequel/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sequel
+package com.sequel.app
 
 import io.flutter.embedding.android.FlutterActivity
 
